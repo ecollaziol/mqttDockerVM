e7548a552bbb193d4a86c224eaec5da940fa1b42
ecollaziol@atlassian.com <ecollaziol@atlassian.com>
2022-09-28 00:31:32 -0300

    Described Docker command

--- scriptServer_Docker.sh
+++ scriptServer_Docker.sh
@@ -5,6 +5,9 @@ echo "####################"
 echo "Starting server inside Docker container and listening on default port 5201"
 
 docker run  -it --rm --name=iperf3-server -p 5201:5201 -p 5201:5201/udp networkstatic/iperf3 -s -V >> "DOCKER_SERVER_$(date +%Y-%m-%d_%H:%M).log"
+# -it: allocate a tty with STDIN for the container process
+# --rm: removes the container once exited
+# --name: name of the container
+# -p 5201:5201 -p 5201:5201/udp – exposes port 5201 on both TCP and UDP modes. Without this, the UDP test fails.
 # -s: server mode
 # -V: verbose mode
-# -p 5201:5201 -p 5201:5201/udp – exposes port 5201 on both TCP and UDP modes. Without this, the UDP test fails.
